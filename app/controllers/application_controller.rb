182923609c35ba7ba641db788032809d260cbf72
Carla Dessi <carla@atech.media>
2019-04-03 14:43:22 +0100

    Admin Menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,35 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   before_action :authorize
   protect_from_forgery with: :exception
 
+  helper_method :admin_user, :current_user
+
+  def admin_user
+    @admin_user ||= User.find session[:user_id] if session[:user_id]
+    if @admin_user.admin
+      @admin_user
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find session[:user_id] if session[:user_id]
+    @current_user
+  end
+
   protected
 
-    def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
-      end
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: 'Please log in'
     end
+  end
 
-    def admin
-      unless User.find_by(id: session[:user_id]).admin
-        redirect_to "/"
-      end
+  def admin
+    unless User.find_by(id: session[:user_id]).admin
+      redirect_to '/'
     end
+  end
 
 end
